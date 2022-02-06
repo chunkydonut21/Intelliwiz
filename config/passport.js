affcc464b6bdaca86c479976a0ae876c3ec1d2b9
chunkydonut21 <shivammahe21@gmail.com>
2022-02-06 15:18:00 +0530

    added user model and configured authentication middleware

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,26 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
 
+// Load User model
+const User = require('../models/User')
+
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
       // Match user
+      try {
+        const user = await User.findOne({ email: email })
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered' })
+        }
+
+        // Match password
+        if (!bcrypt.compareSync(password, appUser.password)) {
+          return done(null, false, { message: 'Password doesnt match' })
+        } else {
+          return done(null, user)
+        }
+      } catch (err) {}
     })
   )
 
